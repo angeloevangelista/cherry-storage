fcc1605c976b91dec58914d7991932003bf600c2
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-09-01 20:27:30 -0300

    Tratando erros

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,37 @@
 /* eslint-disable no-console */
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 
 import './database';
 
 import routes from './routes';
+
+import AppError from './errors/AppError';
 
 console.clear();
 const port = process.env.PORT || 3333;
 const app = express();
 
 app.use(express.json());
-
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.warn(err.message);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error.',
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port} 🚀`);
--- server/src/services/CreateUserService.ts
+++ server/src/services/CreateUserService.ts
@@ -3,6 +3,8 @@ import { hash } from 'bcrypt';
 
 import User from '../models/User';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   name: string;
   surname?: string;
@@ -21,7 +23,7 @@ class CreateUserService {
     });
 
     if (checkIfUserExist) {
-      throw new Error('Email already used');
+      throw new AppError('Email already used');
     }
 
     const hashedPassword = await hash(password, 8);

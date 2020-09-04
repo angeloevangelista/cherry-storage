0ee1f01e3d0f9139980f1d64277f0d3697c39e23
Angelo Evangelista <angeloevan.ane@gmail.com>
2020-09-03 23:31:15 -0300

    Acessando arquivo físico

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import usersRouter from './users.routes';
 import filesRouter from './files.routes';
+import storageRouter from './storage.routes';
 import sessionsRouter from './sessions.routes';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
@@ -9,8 +10,10 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const routes = Router();
 
 routes.use('/users', usersRouter);
-routes.use('/files', ensureAuthenticated, filesRouter);
 routes.use('/sessions', sessionsRouter);
+
+routes.use('/files', ensureAuthenticated, filesRouter);
+routes.use('/storage', ensureAuthenticated, storageRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hey, you call me now 😉' });
---
+++ server/src/routes/storage.routes.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import { Router } from 'express';
+
+import filesUploadConfig from '../config/filesUpload';
+
+import FindFileService from '../services/FindFileService';
+
+const storageRouter = Router();
+
+storageRouter.get('/:file_id', async (request, response) => {
+  const { file_id } = request.params;
+
+  const findFileService = new FindFileService();
+
+  const file = await findFileService.execute({
+    user_id: request.user.id,
+    file_id,
+  });
+
+  if (!file) {
+    return response.status(404).send();
+  }
+
+  const filePath = path.join(filesUploadConfig.directory, file.name);
+
+  console.log(file);
+
+  return response.sendFile(filePath);
+});
+
+export default storageRouter;
